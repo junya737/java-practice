b19d4ba1c7117d0d1bc2ab4909abfc3b97bc67f1
ihiratch <junya737@gmail.com>
2025-01-05 15:55:52 +0900

    Inheritance from car class

--- Class.java
+++ Class.java
@@ -1,9 +1,12 @@
 public class Class {
     public static void main(String[] args) {
-        Car car = new Car("Blue", 60);
-        car.drive();
+        Car toyota = new Car("Red", 100);
+        ElectricCar tesla = new ElectricCar("Blue", 150, 90);
+        toyota.drive();
+        tesla.charge();
+        tesla.drive();
+
     }
-    
 }
 
 class Car{
@@ -18,5 +21,20 @@ class Car{
 
     public void drive(){
         System.out.println(this.color + " car is driving at " + this.speed + " km/h");
+    }
+}
+
+// Electric car. Inherits from Car
+class ElectricCar extends Car{
+    float batteryLevel;
+    // Constructor
+    ElectricCar(String color, float speed, float batteryLevel){
+        // superclassのコンストラクタを再利用
+        super(color, speed);
+        this.batteryLevel = batteryLevel;
+    }
+
+    public void charge(){
+        System.out.println(this.color + " car is charging at " + this.batteryLevel + " %");
     }
 }
