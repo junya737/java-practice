bcaca5729dd994f35ca3e2fdd6e5b4365cd0ce8c
ihiratch <junya737@gmail.com>
2025-01-05 19:51:07 +0900

    Util class

--- TodoList.java
+++ TodoList.java
@@ -1,42 +1,43 @@
+
 import java.util.ArrayList;
 import java.util.List;
 
 
 public class TodoList {
-
-    public static void print(String message) {
-        System.out.println(message);
-    }
-
     public static void main(String[] args) {
         List<Task> tasks = new ArrayList<Task>();
-        Task task1 = new Task("買い物");
-        Task task2 = new Task("掃除");
+        Task task1 = new SimpleTask("買い物");
+        Task task2 = new SimpleTask("掃除");
         task1.markAsDone();
 
         tasks.add(task1);
         tasks.add(task2);
 
         for (Task task : tasks){
-            print(task.getTask()+ " : " + task.getIsDone());
+            Util.print(task.getDetails());
         }
-        
+    }
+}
+
+// Util class
+class Util{
+    public static void print(String message){
+        System.out.println(message);
     }
 }
 
 
-
-class Task{
-    private String task;
+abstract class Task{
+    private String title;
     private boolean isDone;
 
     // Constructor
-    public  Task(String task){
-        this.task = task;
+    public  Task(String title){
+        this.title = title;
         this.isDone = false;
     }
-    public String getTask(){
-        return this.task;
+    public String getTitle(){
+        return this.title;
     }
     public boolean getIsDone(){
         return this.isDone;
@@ -49,6 +50,22 @@ class Task{
     public void markAsNotDone(){
         this.isDone = false;
     }
-
+    
+    public abstract String getDetails();
 }
 
+// simple task
+class SimpleTask extends Task{
+
+    // Constructor
+    public SimpleTask(String title){
+        super(title);
+    }
+    
+    @Override
+    public String getDetails(){
+        return getTitle() + " : " + getIsDone();
+    }
+
+
+}
